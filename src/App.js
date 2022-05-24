cd4f5c563d310b5070f827e27c72dcb6710fdb39
Pedro Moyano <pedromoyano454@gmail.com>
2022-05-24 08:37:24 -0300

    Navbar and react router 6 implemented

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import Companies from './components/Companies';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+
 
 function App() {
-  
-  
+
+
 
   return (
     <div className="App">
       <Container>
-      <Typography variant="h1" gutterBottom>Peter's Jobly app</Typography>
-      <Companies />
+        <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/companies" element={<Companies />} />>
+          </Routes>
+        </BrowserRouter>
       </ Container>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+ const Home = () =>{
+    return <>
+    <Typography variant="h1" gutterBottom>Peter's Jobly app</Typography>
+    </>
+}
+
+export default Home;
